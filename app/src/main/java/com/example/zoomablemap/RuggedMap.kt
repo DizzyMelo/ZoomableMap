9296d7a9c07be6ec0cc85f1e3081d9df38ba5b21
Daniel Melo <daniel.melo42@outlook.com>
2023-06-19 21:27:07 -0500

    code clean up and line scale

--- app/src/main/java/com/example/zoomablemap/RuggedMap.kt
+++ app/src/main/java/com/example/zoomablemap/RuggedMap.kt
@@ -1,17 +1,11 @@
 package com.example.zoomablemap
 
-import android.graphics.Bitmap
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.gestures.rememberTransformableState
 import androidx.compose.foundation.gestures.transformable
-import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -19,15 +13,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
-import com.example.map.R
 import kotlin.math.roundToInt
 
 object RuggedMap {
@@ -39,11 +30,9 @@ object RuggedMap {
         var offsetY by remember { mutableStateOf(0f) }
         var scale by remember { mutableStateOf(1f) }
         var rotation by remember { mutableStateOf(0f) }
-        var offset by remember { mutableStateOf(Offset.Zero) }
-        val state = rememberTransformableState { zoomChange, offsetChange, rotationChange ->
+        val state = rememberTransformableState { zoomChange, _, rotationChange ->
             scale *= zoomChange
             rotation += rotationChange
-            offset += offsetChange
         }
         Canvas(
             modifier = Modifier
@@ -52,10 +41,8 @@ object RuggedMap {
                     scaleX = scale,
                     scaleY = scale,
                     rotationZ = rotation,
-                    translationX = offset.x,
-                    translationY = offset.y
                 )
-                .transformable(state = state, lockRotationOnZoomPan = true, enabled = true)
+                .transformable(state = state, lockRotationOnZoomPan = false, enabled = true)
                 .pointerInput(Unit) {
                     detectDragGestures { change, dragAmount ->
                         change.consume()
@@ -69,7 +56,7 @@ object RuggedMap {
                 color = Color.Blue,
                 topLeft = Offset.Zero,
                 size = size,
-                style = Stroke(width = 1.dp.toPx())
+                style = Stroke(width = 1.dp.div(scale).toPx())
             )
         }
     }
